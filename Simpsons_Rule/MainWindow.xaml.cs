4e5ad31455583830a2ad529b8f3cfec81459936b
Ko4eBHuK <okladnikov.bool@gmail.com>
2020-08-01 15:02:05 +0800

    Добавьте файлы проекта.

---
+++ Simpsons_Rule/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simpsons_Rule
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SolveTheIntegral(object sender, RoutedEventArgs e)
+        {
+            double upperLimitValue = Double.Parse(UpperLimit.Text.Replace(".", ","));
+            double lowerLimitValue = Double.Parse(LowerLimit.Text.Replace(".", ","));
+            double accuracy = Double.Parse(Accuracy.Text.Replace(".", ","));
+            int functionIndex = FunctionsList.SelectedIndex;
+
+            OutputConsole.Text += "Выбрана функция f(x)=";
+            switch (functionIndex)
+            {
+                case 0:
+                    OutputConsole.Text += "x^2+14x-4";
+                    break;
+                case 1:
+                    OutputConsole.Text += "e^(12x)";
+                    break;
+                case 2:
+                    OutputConsole.Text += "x-0.158";
+                    break;
+                case 3:
+                    OutputConsole.Text += "7sin(x-9)";
+                    break;
+            }
+            OutputConsole.Text += "\n";
+
+            var solution = new SimpsonsRuleMethod(lowerLimitValue, upperLimitValue, accuracy, functionIndex);
+
+            OutputConsole.Text += "Приближённое значение интеграла равно ";
+            OutputConsole.Text += Math.Round(solution.IntegralValueN, 8);
+            OutputConsole.Text += "\n";
+
+            OutputConsole.Text += "Погрешность решения равна ";
+            OutputConsole.Text += Math.Round(solution.CalculatedAccuracy, 8);
+            OutputConsole.Text += "\n";
+
+            if(solution.CalculatedAccuracy <= accuracy)
+            {
+                OutputConsole.Text += "Результат достигнут с количеством шагов равным ";
+                OutputConsole.Text += solution.StepCount;
+                OutputConsole.Text += "\n";
+            }
+            else
+            {
+                OutputConsole.Text += "Количество шагов для достижения требуемой точности\nпревышает миллион.\n";
+                OutputConsole.Text += "Метод Симпсона не является оптимальным для данного\nинтеграла на данных пределах";
+                OutputConsole.Text += "\n";
+            }
+
+
+            OutputConsole.Text += "Цена шага равна ";
+            OutputConsole.Text += solution.StepValue;
+
+            OutputConsole.Text += "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+            ConsoleScrollViewer.ScrollToEnd();
+        }
+
+        private void LimitsTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            double reserveLimitValue;
+            TextBox checkedTextBox = (TextBox)sender;
+
+            try
+            {
+                reserveLimitValue = Double.Parse(checkedTextBox.Text.Replace(".",","));
+            }
+            catch
+            {
+                checkedTextBox.Text = "0";
+                MessageBox.Show("Значение для предела выставленно некорректно.\nОно будет сброшено в ноль.");
+            }
+
+        }
+
+        private void Backhitch(object sender, RoutedEventArgs e)
+        {
+            OutputConsole.Text = "";
+            UpperLimit.Text = "1";
+            LowerLimit.Text = "0";
+            FunctionsList.SelectedIndex = 0;
+        }
+    }
+}
---
+++ Simpsons_Rule/SimpsonsRuleMethod.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simpsons_Rule
+{
+    class SimpsonsRuleMethod
+    {
+        public double LowerLimit { get; set; }
+        public double UpperLimit { get; set; }
+        public double IntegralValueN { get; set; }
+        public double IntegralValueTwoN { get; set; }
+        public int StepCount { get; set; }
+        public double StepValue { get; set; }
+        public double CalculatedAccuracy { get; set; }
+        private double Accuracy { get; set; }
+        private double FunctionIndex { get; set; }
+
+
+        public SimpsonsRuleMethod(double incomingLowerLimit, double incomingUpperLimit, double accuracy, int functionIndex)
+        {
+            if(incomingLowerLimit != incomingUpperLimit)
+            {
+                CheckLimits(incomingLowerLimit, incomingUpperLimit);
+                Accuracy = accuracy;
+                FunctionIndex = functionIndex;
+                CalculateIntegralValue();
+            }
+            else
+            {
+                IntegralValueN = 0;
+            }
+        }
+
+        private void CalculateIntegralValue()
+        {
+            IntegralValueN = 0;
+            IntegralValueTwoN = 0;
+            int n = 4;
+
+            do
+            {
+                StepValue = (UpperLimit - LowerLimit) / n;
+
+                IntegralValueN += GetFunctionValueInPoint(LowerLimit);
+                for (int i = 1; i < n; i++)
+                {
+                    IntegralValueN += 4 * GetFunctionValueInPoint(i * StepValue);
+                    i++;
+                    IntegralValueN += 2 * GetFunctionValueInPoint(i * StepValue);
+                }
+                IntegralValueN += GetFunctionValueInPoint(UpperLimit);
+                IntegralValueN *= StepValue;
+                IntegralValueN /= 3;
+
+                n *= 2;
+                StepValue = (UpperLimit - LowerLimit) / n;
+
+                IntegralValueTwoN += GetFunctionValueInPoint(LowerLimit);
+                for (int i = 1; i < n; i++)
+                {
+                    IntegralValueTwoN += 4 * GetFunctionValueInPoint(i * StepValue);
+                    i++;
+                    IntegralValueTwoN += 2 * GetFunctionValueInPoint(i * StepValue);
+                }
+                IntegralValueTwoN += GetFunctionValueInPoint(UpperLimit);
+                IntegralValueTwoN *= StepValue;
+                IntegralValueTwoN /= 3;
+
+                CalculatedAccuracy = Math.Abs(IntegralValueN - IntegralValueTwoN) / 15;
+            }
+            while (n < 1000000 & CalculatedAccuracy >= Accuracy);
+
+            StepCount = n / 2;
+        }
+
+        public double GetFunctionValueInPoint(double x)
+        {
+            double y = 0;
+
+            switch (FunctionIndex)
+            {
+                case 0:
+                    y = Math.Pow(x, 2) + 14 * x - 4;
+                    break;
+                case 1:
+                    y = Math.Pow(Math.E, 12 * x);
+                    break;
+                case 2:
+                    y = x - 0.158;
+                    break;
+                case 3:
+                    y = 7 * Math.Sin(x - 9);
+                    break;
+            }
+
+            return y;
+        }
+
+
+
+        private void CheckLimits(double incomingLowerLimit, double incomingUpperLimit)
+        {
+            if (incomingLowerLimit < incomingUpperLimit)
+            {
+                LowerLimit = incomingLowerLimit;
+                UpperLimit = incomingUpperLimit;
+            }
+            else
+            {
+                UpperLimit = incomingLowerLimit;
+                LowerLimit = incomingUpperLimit;
+            }
+        }
+    }
+}
